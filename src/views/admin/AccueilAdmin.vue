d4f08f23a6211505c2c360e817cac6ce1003ff83
meblo98 <lomouhamedelbachir@gmail.com>
2024-09-20 11:29:06 +0000

    config integration login

---
+++ src/views/admin/AccueilAdmin.vue
@@ -0,0 +1,40 @@
+<template>
+   
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="/src/assets/images/logo.png" class="img-fluid" alt="Sample image" style="width: 100px;">
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2">Features</a></li>
+        <li><a href="#" class="nav-link px-2">Pricing</a></li>
+        <li><a href="#" class="nav-link px-2">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2">About</a></li>
+      </ul>
+
+      <div class="dropdown text-end">
+          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="/src/assets/images/profil.jpeg" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small">
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Deconnexion</a></li>
+          </ul>
+        </div>
+    </header>
+
+</template>
+
+<style lang="css">
+body{
+    background: #EAECF0;
+}
+header{
+    padding: 50px;
+    background: white;
+}
+</style>
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,71 @@
+
+<template>
+   <section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="/src/assets/images/logo.png" class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+
+          <!-- Email input -->
+          <div data-mdb-input-init class="form-outline mb-4">
+            <label class="form-label" for="form3Example3">Email ou Téléphone</label>
+
+            <input type="text" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Entrer votre email ou votre téléphone" />
+          </div>
+
+          <!-- Password input -->
+          <div data-mdb-input-init class="form-outline mb-3">
+            <label class="form-label" for="form3Example4">Mot de passe</label>
+
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Entrer votre mot de passe" />
+          </div>
+
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <router-link  to="/dashboard-admin" >
+              <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;background-color: #522DD1;">Se connecter</button>
+            </router-link>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+
+<style lang="css">
+
+.divider:after,
+.divider:before {
+content: "";
+flex: 1;
+height: 1px;
+background: #eee;
+}
+.h-custom {
+height: calc(100% - 73px);
+}
+@media (max-width: 450px) {
+.h-custom {
+height: 100%;
+}
+}
+
+</style>
+
